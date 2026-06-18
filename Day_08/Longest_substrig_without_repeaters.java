f70489884a580bd3f832ef0c8e807e7c62e1566d
siddh0506 <siddh05102006@gmail.com>
2026-06-18 21:57:21 +0530

    Create Longest_substrig_without_repeaters.java

---
+++ Day_08/Longest_substrig_without_repeaters.java
@@ -0,0 +1,42 @@
+3. Longest Substring Without Repeating Characters
+Medium
+Topics
+premium lock icon
+Companies
+Hint
+Given a string s, find the length of the longest substring without duplicate characters.
+
+ 
+
+Example 1:
+
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3. Note that "bca" and "cab" are also correct answers.
+
+Code:
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+
+        HashSet<Character> set = new HashSet<>();
+
+        int left = 0;
+        int maxLength = 0;
+
+        for(int right = 0; right < s.length(); right++) {
+
+            while(set.contains(s.charAt(right))) {
+                set.remove(s.charAt(left));
+                left++;
+            }
+
+            set.add(s.charAt(right));
+
+            maxLength = Math.max(maxLength,
+                                 right - left + 1);
+        }
+
+        return maxLength;
+    }
+}
