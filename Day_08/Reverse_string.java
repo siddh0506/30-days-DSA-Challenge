f03d6831f65c2910dd73207c0d46c64ac41cb0e3
siddh0506 <siddh05102006@gmail.com>
2026-06-18 21:54:07 +0530

    Create Day_08/Reverse_string.java

---
+++ Day_08/Reverse_string.java
@@ -0,0 +1,29 @@
+344. Reverse String
+
+Hint
+Write a function that reverses a string. The input string is given as an array of characters s.
+
+You must do this by modifying the input array in-place with O(1) extra memory.
+
+ 
+
+Example 1:
+
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+
+Code:
+
+class Solution {
+    public void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length-1;
+        while(left<right){
+            char temp = s[left];
+            s[left]=s[right];
+            s[right]=temp;
+            left++;
+            right--;
+        }
+    }
+}
