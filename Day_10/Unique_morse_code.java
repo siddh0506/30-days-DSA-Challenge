652993979461816f6eee2ff1bea805a9a9530431
siddh0506 <siddh05102006@gmail.com>
2026-06-19 13:20:14 +0530

    Create Day_10/Unique_morse_code.java

---
+++ Day_10/Unique_morse_code.java
@@ -0,0 +1,44 @@
+804. Unique Morse Code Words
+
+International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows:
+
+'a' maps to ".-",
+'b' maps to "-...",
+'c' maps to "-.-.", and so on.
+For convenience, the full table for the 26 letters of the English alphabet is given below:
+
+[".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+Given an array of strings words where each word can be written as a concatenation of the Morse code of each letter.
+
+ 
+
+Example 1:
+
+Input: words = ["gin","zen","gig","msg"]
+Output: 2
+Explanation: The transformation of each word is:
+"gin" -> "--...-."
+"zen" -> "--...-."
+"gig" -> "--...--."
+"msg" -> "--...--."
+There are 2 different transformations: "--...-." and "--...--.".
+
+
+Code :
+
+class Solution {
+    public int uniqueMorseRepresentations(String[] words) {
+      String[] morse = {
+        ".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."
+      };
+      HashSet<String> set = new HashSet<>();
+      for(String word : words){
+        StringBuilder sb = new StringBuilder();
+        for(char c : word.toCharArray()){
+            sb.append(morse[c-'a']);
+        }
+        set.add(sb.toString());
+      }
+      return set.size();
+    }
+}
