f3c154ca3311e232702abdc49ddd945ffe35cdff
siddh0506 <siddh05102006@gmail.com>
2026-06-19 12:03:46 +0530

    Create String_compression.java

---
+++ Day_09/String_compression.java
@@ -0,0 +1,55 @@
+443. String Compression
+
+Hint
+Given an array of characters chars, compress it using the following algorithm:
+
+Begin with an empty string s. For each group of consecutive repeating characters in chars:
+
+If the group's length is 1, append the character to s.
+Otherwise, append the character followed by the group's length.
+The compressed string s should not be returned separately, but instead, be stored in the input character array chars. Note that group lengths that are 10 or longer will be split into multiple characters in chars.
+
+After you are done modifying the input array, return the new length of the array.
+
+You must write an algorithm that uses only constant extra space.
+
+Note: The characters in the array beyond the returned length do not matter and should be ignored.
+
+ 
+
+Example 1:
+
+Input: chars = ["a","a","b","b","c","c","c"]
+Output: 6
+Explanation: The groups are "aa", "bb", and "ccc". This compresses to "a2b2c3".
+After modifying the input array in-place, the first 6 characters of chars should be ["a","2","b","2","c","3"].
+
+
+
+Code:
+
+
+class Solution {
+    public int compress(char[] chars) {
+        int write = 0;
+        int read =0;
+        while(read<chars.length){
+            char current = chars[read];
+            int count =0;
+            while(read<chars.length && chars[read]==current){
+                read++;
+                count++;
+            }
+           chars[write++]=current;
+           if(count>1){
+            for(char c : String.valueOf(count).toCharArray()){
+                chars[write++]=c;
+
+            }
+          
+ } 
+         
+   }
+            return write;
+    }
+}
