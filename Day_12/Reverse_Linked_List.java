a9418083520bdf765275aba6ba7e31d83c16f352
siddh0506 <siddh05102006@gmail.com>
2026-06-22 21:12:30 +0530

    Create Day_12/Reverse_Linked_List.java

---
+++ Day_12/Reverse_Linked_List.java
@@ -0,0 +1,28 @@
+/*
+206. Reverse Linked List
+
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+
+*/
+
+//Code:
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+      ListNode prev = null;
+      ListNode curr = head;
+      while(curr != null){
+        ListNode next = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = next;
+      }  
+      return prev;
+    }
+}
