59ec65dd39c2aef418ed2c7791171b16d660b61f
siddh0506 <siddh05102006@gmail.com>
2026-06-23 22:49:39 +0530

    Create Intersection_Of_Two_Linked_Lists.java

---
+++ Day_13/Intersection_Of_Two_Linked_Lists.java
@@ -0,0 +1,42 @@
+/*
+
+349. Intersection of Two Arrays
+
+Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
+
+ 
+
+Example 1:
+
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2]
+Example 2:
+
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [9,4]
+Explanation: [4,9] is also accepted.
+
+*/
+
+//Code:
+
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+      HashSet<Integer> set1 = new HashSet<>();
+      HashSet<Integer> resultset = new HashSet<>();
+      for(int num : nums1){
+        set1.add(num);
+      }  
+      for(int num : nums2){
+        if(set1.contains(num)){
+            resultset.add(num);
+        }
+      }
+        int[] result = new int[resultset.size()];
+        int i =0;
+        for(int nums : resultset){
+            result[i++]=nums;
+        }
+        return result;
+      }
+    }
