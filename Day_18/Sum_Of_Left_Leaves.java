ab10c12d335b50eba96854554018740c28bcafc9
siddh0506 <siddh05102006@gmail.com>
2026-06-28 20:35:04 +0530

    Create Sum_Of_Left_Leaves.java

---
+++ Day_18/Sum_Of_Left_Leaves.java
@@ -0,0 +1,36 @@
+/*
+
+404. Sum of Left Leaves
+
+Given the root of a binary tree, return the sum of all left leaves.
+
+A leaf is a node with no children. A left leaf is a leaf that is the left child of another node.
+
+ 
+
+Example 1:
+
+
+Input: root = [3,9,20,null,null,15,7]
+Output: 24
+Explanation: There are two left leaves in the binary tree, with values 9 and 15 respectively.
+*/
+
+//Code :
+
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        if(root == null){
+            return 0;
+
+        }
+        int sum =0;
+        if(root.left != null && root.left.left == null && root.left.right == null ){
+            sum += root.left.val;
+        }
+        sum += sumOfLeftLeaves(root.left);
+        sum += sumOfLeftLeaves(root.right);
+
+        return sum;
+    }
+}
