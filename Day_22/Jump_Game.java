9097e3e6fd31a431b4686f94ec0974d1bc53f48e
siddh0506 <siddh05102006@gmail.com>
2026-07-02 21:00:24 +0530

    Create Jump_Game.java

---
+++ Day_22/Jump_Game.java
@@ -0,0 +1,31 @@
+/*
+
+55. Jump Game
+
+You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+
+Return true if you can reach the last index, or false otherwise.
+
+ 
+
+Example 1:
+
+Input: nums = [2,3,1,1,4]
+Output: true
+Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+
+*/
+
+//Code :
+
+class Solution {
+    public boolean canJump(int[] nums) {
+        int  maxreach =0;
+        for(int i =0;i<nums.length;i++){
+            if(i > maxreach)
+            return false;
+            maxreach = Math.max(maxreach,i+nums[i]);
+        }
+        return true;
+    }
+}
