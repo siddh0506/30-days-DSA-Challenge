76e1fd77b4d0dd80e5ceae0bd209840fd69c4cce
siddh0506 <siddh05102006@gmail.com>
2026-07-06 23:07:40 +0530

    Create Day_26/Next_Greater_Element_III.java

---
+++ Day_26/Next_Greater_Element_III.java
@@ -0,0 +1,38 @@
+/*
+
+503. Next Greater Element II
+
+Given a circular integer array nums (i.e., the next element of nums[nums.length - 1] is nums[0]), return the next greater number for every element in nums.
+
+The next greater number of a number x is the first greater number to its traversing-order next in the array, which means you could search circularly to find its next greater number. If it doesn't exist, return -1 for this number.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,1]
+Output: [2,-1,2]
+
+*/
+
+// Code :
+
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+
+        Stack<Integer> stack = new Stack<>();
+        for(int i = 2*n-1;i>=0;i--){
+            int index = i % n;
+            while(!stack.isEmpty() && stack.peek() <= nums[index]){
+                stack.pop();
+            }
+            if(i<n){
+                ans[index]= stack.isEmpty() ? -1 : stack.peek();
+            }
+            stack.push(nums[index]);
+        }
+        return ans;
+    }
+}
