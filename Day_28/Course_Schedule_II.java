1ca70f4aec87f6fd305f1c08807de151d1b442d2
siddh0506 <siddh05102006@gmail.com>
2026-07-11 22:52:02 +0530

    Create Course_Schedule_II.java

---
+++ Day_28/Course_Schedule_II.java
@@ -0,0 +1,53 @@
+/*
+210. Course Schedule II
+
+Hint
+There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
+
+For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
+Return the ordering of courses you should take to finish all courses. If there are many valid answers, return any of them. If it is impossible to finish all courses, return an empty array.
+
+ 
+
+Example 1:
+
+Input: numCourses = 2, prerequisites = [[1,0]]
+Output: [0,1]
+Explanation: There are a total of 2 courses to take. To take course 1 you should have finished course 0. So the correct course order is [0,1].
+*/
+
+//Code:
+
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+       List<List<Integer>> graph = new ArrayList<>();
+       for(int i=0;i<numCourses;i++){
+        graph.add(new ArrayList<>());
+       } 
+       int[] indegree = new int[numCourses];
+       for(int[] pre : prerequisites){
+        graph.get(pre[1]).add(pre[0]);
+        indegree[pre[0]]++;
+       }   
+       Queue<Integer> queue = new LinkedList<>();
+       for(int i = 0; i< numCourses;i++){
+        if(indegree[i]==0)
+        queue.offer(i);
+       }
+       int[] order = new int[numCourses];
+       int index = 0;
+       while(!queue.isEmpty()){
+        int course = queue.poll();
+        order[index++]=course;
+        for(int next : graph.get(course)){
+            indegree[next]--;
+            if(indegree[next]==0)
+            queue.offer(next);
+        }
+       }
+       if(index==numCourses)
+       return order;
+
+       return new int[0];
+    }
+}
